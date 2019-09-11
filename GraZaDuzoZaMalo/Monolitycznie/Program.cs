42c2831a9651480e8f09961043ccbd4b6ef86a6c
FJ8 <55151966+fjakubczak@users.noreply.github.com>
2019-09-11 20:25:45 +0200

    gra

---
+++ GraZaDuzoZaMalo/Monolitycznie/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monolitycznie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 1.komputer losuje liczbe z podanego zakresu
+            // 1 do 100 
+            Random los = new Random();
+            int wylosowana = los.Next(1, 101);
+#if DEBUG
+            Console.WriteLine(wylosowana); // do usuniecia w release
+#endif
+
+            // 2.czlowiek porponuje/ odgaduje 
+            // 3. komputer ocneia propozycje
+            //  koniec powtarzran
+
+            //  4. wypisz staty
+
+
+
+        }
+    }
+}
