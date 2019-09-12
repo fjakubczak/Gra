2e6447f61e906089dea5fdca43e5ec774a299908
FJ8 <55151966+fjakubczak@users.noreply.github.com>
2019-09-12 20:18:47 +0200

    KOD

--- GraZaDuzoZaMalo/Monolitycznie/Program.cs
+++ GraZaDuzoZaMalo/Monolitycznie/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Monolitycznie
 {
@@ -18,14 +19,93 @@ namespace Monolitycznie
 #if DEBUG
             Console.WriteLine(wylosowana); // do usuniecia w release
 #endif
+            Console.WriteLine("Wylosowałem liczbę z zakresu od 1 do 100. Odgadnij ją");
 
-            // 2.czlowiek porponuje/ odgaduje 
-            // 3. komputer ocneia propozycje
+            Stopwatch czas = Stopwatch.StartNew();
+
+          
+
+            //powtarzaj
+            bool odgadniete = false;
+            int licznik = 0;
+
+            do
+            {
+                licznik++;
+
+                // 2.czlowiek porponuje/ odgaduje 
+
+                Console.WriteLine("Podaj swoją propozycję: ");
+                string napis = Console.ReadLine();
+                //zakonczenie
+                if(napis == "koniec")
+                {
+
+                    Console.WriteLine("Szkoda. Że mnie opuszczasz.");
+                    return;
+                }
+
+                //sprawdzanie wyjatkow
+                int propozycja = 0;
+                try
+                {
+                    propozycja = int.Parse(napis); //zgłASZA wyjatek
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Nie podano liczby," + " spróbuj jeszcze raz");
+                    continue;
+
+                }
+
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Przesadziłeś, za duża liczba.");
+                    continue;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Niezidentyfikowany wyjątek. Awaria");
+                    Environment.Exit(1);
+                }
+
+                // 3. komputer ocneia propozycje
+
+                if (propozycja < wylosowana)
+                {
+
+                    Console.WriteLine("Za mało ");
+
+
+                }
+                else if (propozycja > wylosowana)
+                {
+                    Console.WriteLine("Za dużo");
+
+                }
+               
+                
+                else
+                {
+
+                    Console.WriteLine("Brawo, trafiłeś");
+                    // odgadniete = true;
+                    break;
+                }
+            }
+            // while (! odgadniete);
+
+            while (true);
+
+            czas.Stop();
+
+
+
             //  koniec powtarzran
 
             //  4. wypisz staty
-
-
+            Console.WriteLine($"* liczba ruchów: {licznik}");
+            Console.WriteLine($"* czas gry: {czas.Elapsed}");
 
         }
     }
---
+++ GraZaDuzoZaMalo/Proceduralnie/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Proceduralnie
+{
+    class Program
+    {
+        static void Start()
+        {
+            Console.Clear();
+            Console.WriteLine("Aplikacja GRA");
+            Console.WriteLine("=============");
+            Licznik = 0;
+            czas = Stopwatch.StartNew();
+        }
+
+        static int Losuj()
+        {
+            Random los = new Random();
+            int wylosowana = los.Next(1, 101);
+#if DEBUG
+            Console.WriteLine(wylosowana); // do usunięcia w Release
+#endif
+            Console.WriteLine("Wylosowałem liczbę z zakresu od 1 do 100. Odgadnij ją.");
+            return wylosowana;
+        }
+
+        static int WczytajPropozycję()
+        {
+            int propozycja = 0;
+
+            while (true)
+            {
+                Console.Write("Podaj swoją propozycję: ");
+                string napis = Console.ReadLine();
+                if(napis == "koniec")
+                {
+                    throw new ArgumentException("Poddaje się");
+                }
+                try
+                {
+                    propozycja = int.Parse(napis);
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Nie podano liczby.\n Spróbuj jeszcze raz");
+                    continue;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Przesadziłeś. Za duża liczba.");
+                    continue;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Niezidentyfikowany wyjątek. Awaria");
+                    Environment.Exit(1);
+                }
+            }
+
+            Licznik++;
+
+            return propozycja;
+        }
+
+        static bool Ocena(int wylosowana, int propozycja)
+        {
+            if (propozycja < wylosowana)
+            {
+                Console.WriteLine("Za mało");
+
+            }
+            else if (propozycja > wylosowana)
+            {
+                Console.WriteLine("Za dużo");
+
+            }
+
+            else
+            {
+                Console.WriteLine("Trafiłeś");
+                return true;
+
+            }
+            return false;
+        }
+
+        static void Statystyki( )
+        {
+            czas.Stop();
+            Console.WriteLine("Statystyki gry: ");
+            Console.WriteLine($"czas gry: {czas.Elapsed}");
+            Console.WriteLine($"Liczba ruchów: {Licznik}");
+
+        }
+
+        static int Licznik = 0;
+        static Stopwatch czas;
+
+        static void Main(string[] args)
+        {
+            Start();
+            int x = Losuj();
+            bool trafiono;
+            do
+            {
+                int y;
+                try
+                {
+
+                
+                y = WczytajPropozycję();
+                }
+
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("szkoda");
+                    return;
+                
+                }
+                trafiono = Ocena(x, y);
+            }
+            while (!trafiono);
+            Statystyki();
+        }
+
+
+
+
+    }
+}
